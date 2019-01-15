8d6c676fb3e727b390e3da79d164061859775dfc
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-15 12:23:45 +0100

    Update Skript syntax - skript-2019-01-14-1.ps1

--- Skript syntax - skript-2019-01-14-1.ps1
+++ Skript syntax - skript-2019-01-14-1.ps1
@@ -1,11 +1,18 @@
-
+echo "i am creating variables":
 $filename = "C:\Temp\test" + ".txt"
 $filename
 
+#echo "Create a directory "C:\emp and 
+Create a file into the above directory named test.txt":
+
 New-Item -Path $filename  -ItemType File -Force
 
+#echo "Add the current date as text in test.txt":
 Set-Content $filename (get-date)
+echo "Radera innehållet i filen
 
+#Add text "Välkommen till Nackademin" into test.txt":
 Add-Content -value 'Välkommen till Nackademin' $filename 
 
+#echo"Delete the contents of the file":
 Clear-Content $filename 
