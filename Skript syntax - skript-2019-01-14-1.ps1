7a898d907ce62a73fec8820b9306e6f380f3a5c6
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-15 12:28:18 +0100

    Update Skript syntax - skript-2019-01-14-1.ps1

--- Skript syntax - skript-2019-01-14-1.ps1
+++ Skript syntax - skript-2019-01-14-1.ps1
@@ -2,9 +2,7 @@ echo "i am creating variables":
 $filename = "C:\Temp\test" + ".txt"
 $filename
 
-#echo "Create a directory "C:\emp and 
-Create a file into the above directory named test.txt":
-
+#echo "Create a directory "C:\emp and Create a file into the above directory named test.txt":
 New-Item -Path $filename  -ItemType File -Force
 
 #echo "Add the current date as text in test.txt":
