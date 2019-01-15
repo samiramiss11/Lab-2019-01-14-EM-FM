3ec7c3fd047d102021b4a7135eace158524df3f3
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-15 13:21:29 +0100

    Create Skript syntax - skript-2019-01-14-4.ps1

---
+++ Skript syntax - skript-2019-01-14-4.ps1
@@ -0,0 +1,7 @@
+
+
+Kör kommandot "Get-Service | Format-List" och beskriv vad "Format-List" gör.
+  this can format all information about services on computer to a list.
+  
+Kör kommandot "Get-Service | Format-wide" och beskriv vad skillnaden är mot "Format-List".
+   this one formats service but shows only one property of each object. 
