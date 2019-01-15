5624802edcbdc76688e149e32b375b64fe92472c
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-15 12:33:55 +0100

    Update Skript syntax - skript-2019-01-14-2.ps1

--- Skript syntax - skript-2019-01-14-2.ps1
+++ Skript syntax - skript-2019-01-14-2.ps1
@@ -1,18 +1,25 @@
+
+#Create variables.
 $filename = "C:\temp\gronsaker" + ".txt"
 $filename
+
+#Make a list of five different vegetables.Enter two vegetables twice in the list.
 $gronsaklista = "tomat","gurka","morot","salad","Grönkål","tomat","salad"
 
+#Create a file "C: emp gronsaker.txt"
 New-Item -Path $filename  -ItemType File -Force
 
+#Sort the list in alphabetical order and with uniquely occurring vegetables.
 Add-Content -value ($gronsaklista |sort | Get-Unique) $filename 
 
- 
+#how many vegetables the list contains.
 get-content $filename | Measure-Object –word
 
 $list5=(get-content $filename | Measure-Object –word)
 
 $list5=$gronsaklista | Measure
 
+#And add a ending line with how many unique vegetables the list contains.
 add-content -value  ($list5.count) $filename
 
 
