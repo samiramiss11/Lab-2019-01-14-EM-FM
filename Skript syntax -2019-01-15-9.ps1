40fd1835833129490b9b8fcb665c7e053ce09697
samiramiss11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-24 09:21:34 +0100

    Update Skript syntax -2019-01-15-9.ps1

--- Skript syntax -2019-01-15-9.ps1
+++ Skript syntax -2019-01-15-9.ps1
@@ -6,3 +6,6 @@ Get-EventLog -LogName "System" |
 $items = Get-WinEvent -FilterHashtable @{logname="Security"; id=4624;}
 
 Get-EventLog security | Where-Object {$_.TimeGenerated -gt '1/15/19'} | Where-Object {($_.InstanceID -eq 462
+
+
+
